58c267be818869dd8e9e35f50263a7101fcab351
Jihye Lim <73014113+jihye0@users.noreply.github.com>
2024-09-29 16:50:07 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.28 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int ans = num1*1000;
+        ans=ans/num2;
+        System.out.println(ans);
+        //answer=answer*1000;
+        int answer=0;
+        return ans;
+    }
+}
