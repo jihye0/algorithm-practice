4bab73458559ff12cded3f44b4f0961bde754002
Jihye Lim <73014113+jihye0@users.noreply.github.com>
2024-10-09 08:19:57 +0900

    [Silver III] Title: 계단 오르기, Time: 104 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,36 +1,37 @@
-//package DAY03.P2579;
+//package P2579;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	
 	static int N;
-	static int[] stairs;
-	static int[] dp;
+	static int[] score;
+	static int[][] dp;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		N=Integer.parseInt(br.readLine());
-		stairs=new int[N+1];
-		dp=new int[N+1];
-		
-		stairs[0]=0;
-		for(int i=0; i<N; i++) {
-			stairs[i+1]=Integer.parseInt(br.readLine());
-		}
-		
-		dp[0]=0;
-		dp[1]=stairs[1];
-		if(N==1) System.out.println(stairs[1]);
-		else {
-			dp[2]=stairs[1]+stairs[2];
-			for(int i=3; i<=N; i++) {
-				dp[i]=Math.max(dp[i-2]+stairs[i], dp[i-3]+stairs[i-1]+stairs[i]);
-			}
-			System.out.println(dp[N]);
-		}
-	}
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
 
+		score = new int[N+1];
+		dp=new int[N+1][2];
+		for (int i = 0; i < N; i++) {
+			score[i+1] = Integer.parseInt(br.readLine());
+		}
+		if(N==1) {
+			System.out.println(score[1]);
+			return;
+		}
+		dp[1][0]=score[1];
+		dp[1][1]=0;
+		dp[2][0]=score[2];
+		dp[2][1]=score[1]+score[2];
+		for(int i=3; i<=N; i++) {
+			dp[i][1]=dp[i-1][0]+score[i];
+			dp[i][0]=score[i]+Math.max(dp[i-2][0], dp[i-2][1]);
+		}
+		System.out.println(Math.max(dp[N][0], dp[N][1]));
+
+	}
 }
